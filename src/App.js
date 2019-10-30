e7ebaea4a95071a7ef75da56fc8f6060f202cd7b
Francesco Stiffoni <fstiffo@sgajo.com>
2019-10-30 09:28:59 +0100

    Added thanks for granting

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import ErrorBoundary from "./ErrorBoundary";
@@ -54,8 +55,27 @@ const App = () => (
             <Route path="/experiment/:id" component={Experiment} />
           </Switch>
         </Container>
+        <Container className="p-3 footer">
+          <div className="dropdown-divider" />
+          <p class="small">
+            Our grateful thanks to{" "}
+            <a href="https://www.babacova.com/" class="text-info">
+              ALBA cooperativa sociale onlus
+            </a>{" "}
+            &nbsp;
+            <Button
+              href="https://www.facebook.com/albacoop/"
+              variant="outline-info"
+              size="sm"
+            >
+              <i class="fab fa-facebook" />
+            </Button>
+            &nbsp; for the permission to utilize its videos.
+          </p>
+        </Container>
       </div>
     </Router>
   </ErrorBoundary>
 );
+
 export default App;
